b82ea497591277873518f5bbec33c8a16b19ae6e
Dardaul Hoque <rudro1152@gmail.com>
2025-01-01 01:56:16 +0600

    restructure src

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { mount } from 'svelte'
-import './app.css'
-import App from './App.svelte'
+import './ui/app.css'
+import App from './ui/App.svelte'
 
 const app = mount(App, {
   target: document.getElementById('app')!,
