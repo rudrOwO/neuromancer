f9f89dafb697de9fc33a94275f53339a0466c139
Dardaul Hoque <rudro1152@gmail.com>
2025-01-02 01:31:25 +0600

    add custom plugin to vite dev server
    wrong mime type was being served for .wasm files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
-import { defineConfig } from 'vite'
+import fs from "fs";
+import path from "path";
+import { defineConfig, Plugin } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const wasmMimeTypePlugin: Plugin = {
+  name: "wasm-mime-type-plugin",
+  apply: 'serve',
+  configureServer(server) {
+    console.log("wasm-file-server-plugin registered");
+
+    server.middlewares.use(async (req, res, next) => {
+      if (req.url && req.url.endsWith(".wasm")) {
+        console.log("Serving .wasm file from custom plugin", req.url);
+
+        const filePath = path.join(__dirname, "node_modules/onnxruntime-web/dist/ort-wasm-simd-threaded.wasm");
+
+        if (fs.existsSync(filePath)) {
+          res.setHeader("Content-Type", "application/wasm");
+          fs.createReadStream(filePath).pipe(res);
+        } else {
+          console.error("WASM file not found:", filePath);
+          res.statusCode = 404;
+          res.end();
+        }
+      } else {
+        return next();
+      }
+    });
+  },
+};
+
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(), tsconfigPaths()],
+  plugins: [svelte(), tsconfigPaths(), wasmMimeTypePlugin],
 })
