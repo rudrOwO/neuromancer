2e8f2e4ce52ce3aa500708030ad71586e83cfe30
Dardaul Hoque <rudro1152@gmail.com>
2025-01-01 21:25:29 +0600

    test retreiving intermediate results
    Former-commit-id: f96d191071d9b6f37d2195642fb5bd235d8a4be9

--- src/onnx/entrypoint.ts
+++ src/onnx/entrypoint.ts
@@ -1,15 +1,25 @@
-import { Tensor } from 'onnxruntime-web';
+import { Tensor, InferenceSession } from 'onnxruntime-web';
 
-onmessage = (event) => {
+
+onmessage = async (event) => {
   console.log('Received from main thread:', event.data);
 
   // create a [2x3x4] float tensor
   //
-  const buffer01 = new Float32Array(24);
-  buffer01[0] = 0.1; // fill buffer data
-  const tensor01 = new Tensor('float32', buffer01, [2, 3, 4]);
+  const inputTensor = new Tensor('float32', new Float32Array(28 * 28), [1, 1, 28, 28])
+  const modelURL = `${import.meta.env.VITE_PUBLIC_URL}/${import.meta.env.VITE_MODEL_NAME}`
 
+  const outputMap: InferenceSession.NullableOnnxValueMapType = {
+    "Convolution28_Output_0": new Tensor('float32', new Float32Array(8 * 28 * 28), [1, 8, 28, 28]),
+  }
+
+  const session = await InferenceSession.create(modelURL)
+
+  const output = await session.run({
+    "Input3": inputTensor
+  },
+  )
 
   // Respond to the main thread
-  postMessage(tensor01);
+  postMessage(output);
 };
