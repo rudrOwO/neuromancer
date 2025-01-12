3f6594dd5e0fb136b8445011a7159f0ccd53e545
Dardaul Hoque <rudro1152@gmail.com>
2025-01-12 13:44:11 +0600

    add prettierrc
    Former-commit-id: c311f16ba8e67ce7884576eabd6964b3dcbf8018

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-  import DigitInputPanel from "@components/DigitInputPanel.svelte";
-  import ONNXRuntime from "@onnx/entrypoint.ts?worker";
+  import DigitInputPanel from "@components/DigitInputPanel.svelte"
+  import ONNXRuntime from "@onnx/entrypoint.ts?worker"
 
-  const error = $state(false);
+  const error = $state(false)
 
   $effect(() => {
-    const onnxRuntime = new ONNXRuntime();
+    const onnxRuntime = new ONNXRuntime()
     onnxRuntime.onmessage = (event) => {
-      console.log("Received from ONNXRuntime:", event.data);
-    };
-
-    const modelURL = `${import.meta.env.VITE_PUBLIC_URL}/${import.meta.env.VITE_MODEL_NAME}`;
-    try {
-      onnxRuntime.postMessage({ modelURL });
-    } catch (error) {
-      error = true;
+      console.log("Received from ONNXRuntime:", event.data)
     }
-  });
+
+    const modelURL = `${import.meta.env.VITE_PUBLIC_URL}/${import.meta.env.VITE_MODEL_NAME}`
+    try {
+      onnxRuntime.postMessage({ modelURL })
+    } catch (error) {
+      error = true
+    }
+  })
 </script>
 
 <main class="bg-slate-700 grid place-content-center h-screen">
