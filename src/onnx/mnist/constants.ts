49531b675a60f84c3fc2bc0e75486713b33fb4f1
Dardaul Hoque <rudro1152@gmail.com>
2025-01-14 16:28:25 +0600

    add output node names
    Former-commit-id: 164179c85e299bb9dc455aa3cc82d31639f29572

--- src/onnx/mnist/constants.ts
+++ src/onnx/mnist/constants.ts
@@ -1,2 +1,7 @@
 export const modelURL = `${import.meta.env.VITE_PUBLIC_URL}/mnist-12.onnx`
 export const inputDimension = [1, 1, 28, 28]
+export const orderedOutputNodes = [
+  "Pooling66_Output_0",
+  "Pooling160_Output_0",
+  "Plus214_Output_0",
+]
