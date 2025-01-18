e4fc3c210d47ebfe3181efb34125a035fee44028
Dardaul Hoque <rudro1152@gmail.com>
2025-01-18 17:21:23 +0600

    move postprocessing to OutputPanel component

--- src/ui/App.svelte
+++ src/ui/App.svelte
@@ -1,34 +1,14 @@
 <script lang="ts">
   import DigitInputPanel from "@components/DigitInputPanel.svelte"
-  import { softmax } from "@utils/math"
+  import OutputPanel from "@components/OutputPanel.svelte"
   import type { InferenceResponse } from "shared/bridge"
   import { initializeModel } from "shared/bridge"
-  import {
-    FINAL_NODE,
-    INPUT_TENSOR_DIMENSION,
-    MODEL_URL,
-  } from "shared/constants/mnist"
+  import { INPUT_TENSOR_DIMENSION, MODEL_URL } from "shared/constants/mnist"
 
   let isError = $state(false)
   let isInitializing = $state(true)
 
   let inferenceResponse = $state<InferenceResponse | null>(null)
-  let prediction = $derived.by(function postProcess() {
-    if (inferenceResponse != null) {
-      return softmax(
-        Array.from(inferenceResponse.outputNodes[FINAL_NODE].tensorData),
-      )
-    } else {
-      return null
-    }
-  })
-
-  // DEBUG   Logger
-  $effect(function log() {
-    prediction?.map((value, index) => {
-      console.log(`${index}: ${value}`)
-    })
-  })
 
   $effect(function ready() {
     if (!isInitializing) {
@@ -49,4 +29,5 @@
 
 <main class="bg-slate-700 grid place-content-center h-screen">
   <DigitInputPanel bind:inferenceResponse />
+  <OutputPanel {inferenceResponse} />
 </main>
