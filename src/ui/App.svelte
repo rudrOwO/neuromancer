b85905a9e37fca542088d1d89ef0d20ae7af1119
Dardaul Hoque <rudro1152@gmail.com>
2025-01-22 09:55:37 +0600

    use special svelte syntax for resolving promise

--- src/ui/App.svelte
+++ src/ui/App.svelte
@@ -5,32 +5,24 @@
   import { initializeModel } from "bridge"
   import { INPUT_TENSOR_DIMENSION, MODEL_URL } from "ui/constants/mnist"
 
-  let isError = $state(false)
-  let isInitializing = $state(true)
-
   let inferenceResponse = $state<InferenceResponse | null>(null)
-
-  $effect(() => {
-    if (!isInitializing) {
-      console.log("Model Initialzed")
-    }
-  })
-
-  $effect(() => {
-    initializeModel(MODEL_URL, INPUT_TENSOR_DIMENSION)
-      .then((_) => {
-        isInitializing = false
-      })
-      .catch((_) => {
-        isError = true
-      })
-  })
 </script>
 
 <main class="bg-background h-screen">
-  <!-- TODO  Add Loading and Error screens -->
-  <div class="flex flex-col justify-evenly fixed top-1/2 -translate-y-1/2 ml-4">
-    <DigitInputPanel bind:inferenceResponse />
-    <OutputPanel {inferenceResponse} />
-  </div>
+  {#await initializeModel(MODEL_URL, INPUT_TENSOR_DIMENSION)}
+    {@render placeholder("Initiliazing Model...")}
+  {:then}
+    <div
+      class="flex flex-col justify-evenly fixed top-1/2 -translate-y-1/2 ml-4"
+    >
+      <DigitInputPanel bind:inferenceResponse />
+      <OutputPanel {inferenceResponse} />
+    </div>
+  {:catch error}
+    {@render placeholder(`Something went wrong: ${error.message}`)}
+  {/await}
 </main>
+
+{#snippet placeholder(message: string)}
+  <p class="text-secondary text-4xl">{message}</p>
+{/snippet}
