78ac65d1d4daaeceee6b07f7c9e8729329e03602
Dardaul Hoque <rudro1152@gmail.com>
2025-01-23 13:33:38 +0600

    rescale input tensor for rendering

--- src/ui/App.svelte
+++ src/ui/App.svelte
@@ -6,7 +6,7 @@
   import { INPUT_TENSOR_DIMENSION, MODEL_URL } from "ui/constants/mnist"
 
   let inferenceResponse = $state<InferenceResponse | null>(null)
-  let inputTensorData = $state<Float32Array | null>(null)
+  let renderTensorData = $state<Float32Array | null>(null)
 </script>
 
 <main class="bg-background h-screen">
@@ -16,7 +16,7 @@
     <div
       class="flex flex-col justify-evenly fixed top-1/2 -translate-y-1/2 ml-4"
     >
-      <DigitInputPanel bind:inferenceResponse bind:inputTensorData />
+      <DigitInputPanel bind:inferenceResponse bind:renderTensorData />
       <OutputPanel {inferenceResponse} />
     </div>
   {:catch error}
