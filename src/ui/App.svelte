d5e6d254a17cb346eedb922ae6ac7cc6efde6c1a
Dardaul Hoque <rudro1152@gmail.com>
2025-02-01 04:01:11 +0600

    fix z index orders
    Former-commit-id: f046e77d3d2b46bc186b57188ace8d2a80a642b3

--- src/ui/App.svelte
+++ src/ui/App.svelte
@@ -4,24 +4,27 @@
   import type { InferenceResponse } from "bridge"
   import { initializeModel } from "bridge"
   import { INPUT_TENSOR_DIMENSION, MODEL_URL } from "ui/constants/mnist"
+  import NeuralNetwork from "@3d/NeuralNetwork.svelte"
+  import { Canvas } from "@threlte/core"
 
   let inferenceResponse = $state<InferenceResponse | null>(null)
   let renderTensorData = $state<Float32Array | null>(null)
 </script>
 
-<!-- NOTE  <main> is target for 3D rendering -->
 <main class="bg-black h-screen flex">
   {#await initializeModel(MODEL_URL, INPUT_TENSOR_DIMENSION)}
     {@render placeholder("Initiliazing Model...")}
   {:then}
     <OutputPanel {inferenceResponse} />
     <DigitCanvas bind:inferenceResponse bind:renderTensorData />
+    <Canvas>
+      <NeuralNetwork />
+    </Canvas>
   {:catch error}
     {@render placeholder(`Something went wrong: ${error.message}`)}
   {/await}
 </main>
 
-<!-- TODO  Add proper Loading screen and error screen -->
 {#snippet placeholder(message: string)}
   <p class="fixed grid place-items-center inset-0 text-text-color text-4xl">
     <span>{message}</span>
--- src/ui/components/OutputPanel.svelte
+++ src/ui/components/OutputPanel.svelte
@@ -31,7 +31,7 @@
 </script>
 
 <div
-  class="flex flex-col fixed bg-background w-9 sm:w-12 h-[95vh] self-center ml-2 rounded-lg overflow-hidden z-10"
+  class="flex flex-col fixed bg-background w-9 sm:w-12 h-[95vh] self-center ml-2 rounded-lg overflow-hidden z-100"
 >
   {#each predictions as p, index}
     <PredictionBar label={index} flexValue={p} />
