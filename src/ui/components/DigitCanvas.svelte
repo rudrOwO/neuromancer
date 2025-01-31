77f7e4f8f75ab0fa4d9d0a8242f9ad1e48be11f8
Dardaul Hoque <rudro1152@gmail.com>
2025-01-31 21:36:06 +0600

    bugfix: drawing state persistence

--- src/ui/components/DigitCanvas.svelte
+++ src/ui/components/DigitCanvas.svelte
@@ -19,10 +19,11 @@
     renderTensorData = $bindable(),
   }: Props = $props()
 
+  let isUIVisible = true
+
   let canvas: HTMLCanvasElement
   let canvasScaled: HTMLCanvasElement
   let canvasCenterCrop: HTMLCanvasElement
-
   let ctx: CanvasRenderingContext2D
   let ctxCenterCrop: CanvasRenderingContext2D
   let ctxScaled: CanvasRenderingContext2D
@@ -30,13 +31,7 @@
   let strokes: any = []
   let isDrawing = false
   let isThrottled = false
-  let isUIVisible = $state(true)
-
-  $effect(() => {
-    ctx = canvas.getContext("2d", { willReadFrequently: true })!
-    ctxCenterCrop = canvasCenterCrop.getContext("2d")!
-    ctxScaled = canvasScaled.getContext("2d", { willReadFrequently: true })!
-  })
+  let canvasHeight = $state("min-content")
 
   const preProcess = (): {
     activationTensorData: Float32Array
@@ -97,6 +92,12 @@
   }
 
   const toggleUI = () => {
+    if (isUIVisible) {
+      canvasHeight = "0px"
+    } else {
+      canvasHeight = "min-content"
+    }
+
     isUIVisible = !isUIVisible
   }
 
@@ -161,67 +162,82 @@
     })
     isThrottled = true
   }
+
+  $effect(() => {
+    ctx = canvas.getContext("2d", { willReadFrequently: true })!
+    ctxCenterCrop = canvasCenterCrop.getContext("2d")!
+    ctxScaled = canvasScaled.getContext("2d", { willReadFrequently: true })!
+  })
 </script>
+
+<div
+  class="flex flex-col fixed w-[300px] max-h-min right-2 bottom-[2.5vh] sm:right-6 rounded-lg overflow-hidden z-10"
+  style:height={canvasHeight}
+>
+  <div
+    class="w-full text-center flex justify-center items-center text-text-color text-2xl p-3 bg-accent-0 shadow-xl"
+  >
+    <img class="h-8 mx-2" src="/pen-icon.svg" alt="Pen Icon" />
+    Draw a Digit (0-9)
+  </div>
+  <canvas
+    bind:this={canvas}
+    class="cursor-crosshair bg-background text-text-color border-gray-400 hover:border-2"
+    id="input-canvas"
+    width="300"
+    height="300"
+    onmousedown={startDraw}
+    onmouseup={stopDraw}
+    onmouseleave={stopDraw}
+    onmousemove={handleMouseMove}
+    ontouchstart={startDraw}
+    ontouchend={stopDraw}
+    ontouchmove={handleMouseMove}
+  ></canvas>
+  <canvas
+    bind:this={canvasScaled}
+    id="input-canvas-scaled"
+    width="28"
+    height="28"
+    style="display: none"
+  ></canvas>
+  <canvas
+    bind:this={canvasCenterCrop}
+    id="input-canvas-centercrop"
+    style="display: none"
+  ></canvas>
+  <div class="flex">
+    <Button
+      onclick={toggleUI}
+      iconSrc="/hide-icon.svg"
+      altText="Hide"
+      text="Hide"
+      type="hide"
+    />
+    <Button
+      onclick={clear}
+      iconSrc="/clear-icon.svg"
+      altText="Clear Button"
+      text="Clear"
+      type="clear"
+    />
+  </div>
+</div>
 
 <div
   class="flex flex-col fixed w-[300px] max-h-min right-2 bottom-[2.5vh] sm:right-6 rounded-lg overflow-hidden"
 >
-  {#if isUIVisible}
-    <div
-      class="w-full text-center flex justify-center items-center text-text-color text-2xl p-3 bg-accent-0 shadow-xl"
-    >
-      <img class="h-8 mx-2" src="/pen-icon.svg" alt="Pen Icon" />
-      Draw a Digit (0-9)
-    </div>
-    <canvas
-      bind:this={canvas}
-      class="cursor-crosshair bg-background text-text-color border-gray-400 hover:border-2"
-      id="input-canvas"
-      width="300"
-      height="300"
-      onmousedown={startDraw}
-      onmouseup={stopDraw}
-      onmouseleave={stopDraw}
-      onmousemove={handleMouseMove}
-      ontouchstart={startDraw}
-      ontouchend={stopDraw}
-      ontouchmove={handleMouseMove}
-    ></canvas>
-    <canvas
-      bind:this={canvasScaled}
-      id="input-canvas-scaled"
-      width="28"
-      height="28"
-      style="display: none"
-    ></canvas>
-    <canvas
-      bind:this={canvasCenterCrop}
-      id="input-canvas-centercrop"
-      style="display: none"
-    ></canvas>
-    <div class="flex">
-      <Button
-        onclick={toggleUI}
-        iconSrc="/hide-icon.svg"
-        altText="Hide"
-        text="Hide"
-        type="hide"
-      />
-      <Button
-        onclick={clear}
-        iconSrc="/clear-icon.svg"
-        altText="Clear Button"
-        text="Clear"
-        type="clear"
-      />
-    </div>
-  {:else}
-    <Button
-      onclick={toggleUI}
-      iconSrc="/canvas-icon.svg"
-      altText="Show Canvas"
-      text="Show Canvas"
-      type="default"
-    />
-  {/if}
+  <Button
+    onclick={toggleUI}
+    iconSrc="/canvas-icon.svg"
+    altText="Show Canvas"
+    text="Show Canvas"
+    type="default"
+  />
 </div>
+
+<style>
+  div {
+    transition: height 1s linear;
+  }
+</style>
