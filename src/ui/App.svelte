301f1aeed303acc31eee0c2aedc08a7428b4bb2a
Dardaul Hoque <rudro1152@gmail.com>
2025-02-01 14:14:35 +0600

    add dynamic import to lazy load 3D Neural Network

--- src/ui/App.svelte
+++ src/ui/App.svelte
@@ -4,22 +4,18 @@
   import type { InferenceResponse } from "bridge"
   import { initializeModel } from "bridge"
   import { INPUT_TENSOR_DIMENSION, MODEL_URL } from "ui/constants/mnist"
-  import NeuralNetwork from "@3d/NeuralNetwork.svelte"
-  import { Canvas } from "@threlte/core"
 
   let inferenceResponse = $state<InferenceResponse | null>(null)
   let renderTensorData = $state<Float32Array | null>(null)
 </script>
 
 <main class="bg-black h-screen flex">
-  {#await initializeModel(MODEL_URL, INPUT_TENSOR_DIMENSION)}
+  {#await initializeModel(MODEL_URL, INPUT_TENSOR_DIMENSION).then( (_) => import("@3d/NeuralNetwork.svelte"), )}
     {@render placeholder("Initiliazing Model...")}
-  {:then}
+  {:then { default: NeuralNetowork }}
     <OutputPanel {inferenceResponse} />
     <DigitCanvas bind:inferenceResponse bind:renderTensorData />
-    <Canvas>
-      <NeuralNetwork />
-    </Canvas>
+    <NeuralNetowork />
   {:catch error}
     {@render placeholder(`Something went wrong: ${error.message}`)}
   {/await}
