bc164376678f72b86640f5784795a511e537c5b0
Dardaul Hoque <rudro1152@gmail.com>
2025-02-03 22:34:30 +0600

    add cursor style to 3d canvas

--- src/ui/components/3d/MNIST.svelte
+++ src/ui/components/3d/MNIST.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
   import { T } from "@threlte/core"
+  import { Grid, OrbitControls } from "@threlte/extras"
+  import type { InferenceResponse } from "bridge"
+
+  type Props = {
+    inferenceResponse: InferenceResponse | null
+  }
+
+  const { inferenceResponse }: Props = $props()
 </script>
 
-<T.PerspectiveCamera
-  position={[10, 10, 10]}
-  oncreate={(ref) => {
-    ref.lookAt(0, 0, 0)
-  }}
+<T.PerspectiveCamera makeDefault position={[0, 100, 200]} fov={60}>
+  <OrbitControls enableDamping />
+</T.PerspectiveCamera>
+
+<Grid
+  infiniteGrid
+  sectionColor="#4a4b4a"
+  sectionSize={20}
+  cellSize={10}
+  fadeDistance={250}
 />
 
-<T.Mesh>
-  <T.BoxGeometry args={[1, 1, 1]} />
-  <T.MeshBasicMaterial color="red" />
-</T.Mesh>
+<T.AmbientLight color="#fff" intensity={0.4} />
+<!-- <T.DirectionalLight position={[0, 200, 200]} intensity={2} color="#fff" /> -->
+<!-- <T.DirectionalLight position={[0, 200, -200]} color="#fff" intensity={2} /> -->
