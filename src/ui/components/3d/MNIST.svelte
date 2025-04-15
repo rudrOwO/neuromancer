46aecbcd3f98485d186bf5e48d41a6ae392982fc
Dardaul Hoque <rudro1152@gmail.com>
2025-04-15 12:02:43 +0600

    add activation maps
    Former-commit-id: bd3463bbbf0222701c5f21d406f005eeadf2f05f

--- src/ui/components/3d/MNIST.svelte
+++ src/ui/components/3d/MNIST.svelte
@@ -3,6 +3,7 @@
   import { Align, Gizmo, OrbitControls } from "@threlte/extras"
   import type { InferenceResponse } from "bridge"
   import Tensor2D from "@3d/Tensor2D.svelte"
+  import ActivationMap from "./ActivationMap.svelte"
   import { AMBIENT_LIGHT_INTENSITY } from "@constants/global"
 
   type Props = {
@@ -15,7 +16,7 @@
     $props()
 </script>
 
-<T.PerspectiveCamera makeDefault position={[0, 0, 200]} fov={25}>
+<T.PerspectiveCamera makeDefault position={[0, 0, 400]} fov={25}>
   <OrbitControls enableDamping>
     <Gizmo placement="top-right" size={150} />
   </OrbitControls>
@@ -23,11 +24,28 @@
 
 <T.AmbientLight color="#fff" intensity={AMBIENT_LIGHT_INTENSITY} />
 
-<Align>
+<Align x={0} y={0} z={false}>
   <Tensor2D
     position={[0, 0, 0]}
+    pointSize={6}
     rows={inputTensorDimension[2]}
     columns={inputTensorDimension[3]}
     tensorData={inputTensorData}
   />
 </Align>
+
+<ActivationMap
+  z={-100}
+  {...inferenceResponse.orderedOutputNodes[0]}
+  rowLength={4}
+  pointSize={8}
+  gap={50}
+/>
+
+<ActivationMap
+  z={-200}
+  {...inferenceResponse.orderedOutputNodes[1]}
+  rowLength={4}
+  pointSize={12}
+  gap={50}
+/>
