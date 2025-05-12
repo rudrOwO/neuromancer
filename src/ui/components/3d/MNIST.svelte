3e9a9ad3a85842b966fbc8d179d8a5d020d00ea4
Dardaul Hoque <rudro1152@gmail.com>
2025-05-12 23:27:59 +0600

    add interactivity to ActivationMaps
    Former-commit-id: 37fa3f9e6207eeb588507cf4604cf8a8121ca1c3

--- src/ui/components/3d/MNIST.svelte
+++ src/ui/components/3d/MNIST.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { T } from "@threlte/core"
-  import { OrbitControls } from "@threlte/extras"
+  import { OrbitControls, interactivity } from "@threlte/extras"
   import type { InferenceResponse } from "bridge"
   import TensorGrid from "@3d/TensorGrid.svelte"
   import { AMBIENT_LIGHT_INTENSITY } from "@constants/global"
@@ -11,10 +11,15 @@
     inferenceResponse: InferenceResponse
   }
 
-  let interactionMutexLock = $state(0)
-
   const { inputTensorDimension, inputTensorData, inferenceResponse }: Props =
     $props()
+
+  interactivity({
+    filter: (hits, _) => {
+      // Only return the first hit from the raycaster
+      return hits.slice(0, 1)
+    },
+  })
 </script>
 
 <T.PerspectiveCamera makeDefault position={[375, 275, 375]} fov={25}>
@@ -24,15 +29,13 @@
 <T.AmbientLight color="#fff" intensity={AMBIENT_LIGHT_INTENSITY} />
 
 <TensorGrid
-  z={120}
+  z={110}
   name="Input"
   dimension={inputTensorDimension}
   activationMaps={[inputTensorData]}
   numberOfColumns={1}
   pointSize={6}
   gap={0}
-  bind:interactionMutexLock
-  interactionMutexKey={0b1}
 />
 
 <TensorGrid
@@ -42,17 +45,13 @@
   numberOfColumns={4}
   pointSize={8}
   gap={50}
-  bind:interactionMutexLock
-  interactionMutexKey={0b10}
 />
 
 <TensorGrid
-  z={-120}
+  z={-110}
   name="Convolution Layer #2"
   {...inferenceResponse.orderedOutputNodes[1]}
   numberOfColumns={4}
   pointSize={12}
   gap={40}
-  bind:interactionMutexLock
-  interactionMutexKey={0b100}
 />
