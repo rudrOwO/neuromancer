5c45f7e0f95982e8cd2b679f65e7c942e20032e3
Dardaul Hoque <rudro1152@gmail.com>
2025-05-25 19:49:57 +0600

    make individaul tensors clickable
    Former-commit-id: ea5f416050dc9c0e6ed13d5f56ef4b63871a7115

--- src/ui/components/3d/TensorGrid.svelte
+++ src/ui/components/3d/TensorGrid.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import type { OutputNode } from "bridge"
   import Tensor2D from "@3d/Tensor2D.svelte"
-  import { Text, Align, useCursor } from "@threlte/extras"
-  import { TENSOR_FONT_SIZE, TENSOR_ZOOM_CONSTANT } from "@constants/global"
-  import { T } from "@threlte/core"
+  import { Text, Align } from "@threlte/extras"
+  import {
+    TENSOR_FONT_SIZE,
+    TENSOR_ZOOM_CONSTANT,
+    TEXT_COLOR,
+  } from "@constants/global"
 
   type Props = OutputNode & {
     name: string
@@ -11,8 +14,6 @@
     numberOfColumns: number
     gap: number
     pointSize: number
-    isPointerDown: boolean
-    isPointerDragging: boolean
   }
 
   let {
@@ -23,54 +24,22 @@
     dimension,
     activationMaps,
     z,
-    isPointerDragging = $bindable(),
-    isPointerDown = $bindable(),
   }: Props = $props()
 
-  const numberOfRows = activationMaps.length / numberOfColumns
-
   const tensorWidth = dimension[2] * TENSOR_ZOOM_CONSTANT * pointSize
-  const tensorHeight = dimension[3] * TENSOR_ZOOM_CONSTANT * pointSize
 
   // Calculate midpoint for rendering ActivationMap's name
   // because <Align> isn't working for <Text> 🤷
   const rightBound = gap * (numberOfColumns / 2)
   const leftBound = gap * (numberOfColumns / 2 - 1) + tensorWidth
   const midPoint = rightBound + (leftBound - rightBound) / 2
-
-  const activationMapWidth = gap * (numberOfColumns - 1) + tensorWidth
-  const activationMapHeight = gap * (numberOfRows - 1) + tensorHeight
-
-  let textColor = $state("gray")
-
-  const { onPointerEnter, onPointerLeave } = useCursor()
-
-  function handlePointerRelease() {
-    if (!isPointerDragging) {
-      // TODO  Toggle modal here
-      alert(`Modal Triggered for ${name}`)
-    }
-
-    isPointerDown = false
-    isPointerDragging = false
-  }
-
-  function handlePointerOver() {
-    onPointerEnter()
-    textColor = "white"
-  }
-
-  function handlePointerOut() {
-    onPointerLeave()
-    textColor = "gray"
-  }
 </script>
 
 <Align x={0} y={0} z={false}>
   <Text
     position={[midPoint, 3, z]}
     text={name}
-    color={textColor}
+    color={TEXT_COLOR}
     fontSize={TENSOR_FONT_SIZE}
     anchorX="center"
     anchorY="bottom"
@@ -88,22 +57,4 @@
       tensorData={map}
     />
   {/each}
-  <T.Mesh
-    position={[activationMapWidth / 2, -activationMapHeight / 2, z - 2]}
-    onpointerup={handlePointerRelease}
-    onpointerover={handlePointerOver}
-    onpointerout={handlePointerOut}
-  >
-    <T.PlaneGeometry args={[activationMapWidth, activationMapHeight]} />
-    <T.MeshBasicMaterial
-      args={[
-        {
-          color: 0x000000,
-          side: 2,
-          transparent: true,
-          opacity: 0.6,
-        },
-      ]}
-    />
-  </T.Mesh>
 </Align>
