c91a5777613d32a5740f6876c172b0c21ecd1f09
Dardaul Hoque <rudro1152@gmail.com>
2025-05-29 10:11:10 +0600

    rename globals => graphics

--- src/ui/constants/mnist.ts
+++ src/ui/constants/mnist.ts
@@ -1,4 +1,4 @@
-import { DEFAULT_GRAY_VALUE } from "@constants/global"
+import { DEFAULT_GRAY_VALUE } from "@constants/graphics"
 import type { InferenceResponse, OutputNode } from "bridge"
 
 export const MODEL_URL = `${import.meta.env.VITE_PUBLIC_URL}/mnist-12.onnx`
