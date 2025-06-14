68ed417a1750c734f9bdb9363790ccce3bb4c7c3
Dardaul Hoque <rudro1152@gmail.com>
2025-06-14 14:46:14 +0600

    add kernel

--- src/ui/components/infographics/InfographicsModal.svelte
+++ src/ui/components/infographics/InfographicsModal.svelte
@@ -1,40 +1,60 @@
 <script lang="ts">
   import {
     infographicsModal,
-    closeModal, // TODO  Add this event handler
+    closeModal,
     tensorState,
   } from "@sharedstate/infographics.svelte"
   import UnmaskedTensor from "./UnmaskedTensor.svelte"
 
-  // TODO  Add this event handler
   function preventClickEventPropagation(e: MouseEvent) {
     e.stopPropagation()
   }
+
+  function handleEscapeKey(e: KeyboardEvent) {
+    if (e.key === "Escape") {
+      e.preventDefault()
+      closeModal()
+    }
+  }
+
+  $effect(() => {
+    infographicsModal.element!.addEventListener("keydown", handleEscapeKey)
+
+    return () => {
+      infographicsModal.element!.removeEventListener("keydown", handleEscapeKey)
+    }
+  })
 </script>
 
 <dialog
   class="m-auto bg-background outline-0"
   bind:this={infographicsModal.element}
+  onclick={closeModal}
 >
-  <div class="grid place-items-center">
-    <span class="text-white my-2">InfoGraphics Modal</span>
+  <div class="grid place-items-center" onclick={preventClickEventPropagation}>
+    <span class="text-white text-xl font-bold my-4"
+      >{infographicsModal.layerName}</span
+    >
 
-    <div class="flex flex-row">
-      <!-- Unmasked tensors -->
-      <div
-        class="grid grid-flow-col [grid-template-rows:repeat(4,min-content)] gap-4"
-      >
-        {#each tensorState.unmaskedTensors as tensor}
-          <UnmaskedTensor
-            tensorData={tensor.tensorData}
-            rows={tensor.rows}
-            columns={tensor.columns}
-            size={tensor.size}
-          ></UnmaskedTensor>
-        {/each}
+    {#if infographicsModal.isOpen}
+      <div class="flex flex-col lg:flex-row">
+        <div
+          class="grid grid-flow-col [grid-template-rows:repeat(4,min-content)] gap-4"
+        >
+          {#each tensorState.unmaskedTensors as tensor}
+            <UnmaskedTensor
+              tensorData={tensor.tensorData}
+              rows={tensor.rows}
+              columns={tensor.columns}
+              grayBoxSize={tensor.grayBoxSize}
+              kernelStride={tensor.kernelStride}
+              kernelDimension={tensor.kernelDimension}
+            ></UnmaskedTensor>
+          {/each}
+        </div>
+
+        <span class="text-white my-2">Masked tensor</span>
       </div>
-
-      <span class="text-white my-2">Masked tensor</span>
-    </div>
+    {/if}
   </div>
 </dialog>
