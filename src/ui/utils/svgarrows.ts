afc8bb87ae7a5d651674abc54bd686b94f5b4497
Dardaul Hoque <rudro1152@gmail.com>
2025-06-16 12:51:54 +0600

    implement svg arrows in inforgraphics

--- src/ui/utils/svgarrows.ts
+++ src/ui/utils/svgarrows.ts
@@ -1,7 +1,6 @@
-export function getArrowSourceCoordinates(
-  el: HTMLDivElement | HTMLImageElement,
-  parent: HTMLDivElement,
-) {
+import { SVG } from "@svgdotjs/svg.js"
+
+function getArrowSourceCoordinates(el: HTMLElement, parent: HTMLElement) {
   const rect = el.getBoundingClientRect()
   const parentRect = parent.getBoundingClientRect()
 
@@ -11,15 +10,30 @@ export function getArrowSourceCoordinates(
   }
 }
 
-export function getArrowTargetCoordinates(
-  el: HTMLDivElement | HTMLImageElement,
-  parent: HTMLDivElement,
-) {
+function getArrowTargetCoordinates(el: HTMLElement, parent: HTMLElement) {
   const rect = el.getBoundingClientRect()
   const parentRect = parent.getBoundingClientRect()
 
   return {
     x: rect.left - parentRect.left,
     y: rect.top - parentRect.top + rect.height / 2,
+  }
+}
+
+export function initializeSVG(parent: HTMLElement) {
+  const draw = SVG().addTo(parent).size("100%", "100%")
+
+  return function drawArrow(source: HTMLElement, target: HTMLElement) {
+    const sourceCoordinates = getArrowSourceCoordinates(source, parent)
+    const targetCoordinates = getArrowTargetCoordinates(target, parent)
+
+    return draw
+      .line(
+        sourceCoordinates.x,
+        sourceCoordinates.y,
+        targetCoordinates.x,
+        targetCoordinates.y,
+      )
+      .stroke({ width: 2, dasharray: "6 6" })
   }
 }
