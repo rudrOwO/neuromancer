cc97cdab4089ba128706c125b292e98a6048389b
Dardaul Hoque <rudro1152@gmail.com>
2025-07-05 16:11:05 +0600

    wip: Reveal arrows between tensors in 3d space upon hovering

---
+++ src/ui/components/3d/InferenceFlow.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { getCurrentFlow } from "@sharedstate/inferenceflow.svelte"
+  import { T } from "@threlte/core"
+  import { MeshLineGeometry, MeshLineMaterial } from "@threlte/extras"
+</script>
+
+<!-- TODO  Add Dash animation -->
+{#each getCurrentFlow() as edge}
+  <T.Mesh>
+    <MeshLineGeometry points={edge} />
+    <MeshLineMaterial width={1.0} color="gray" />
+  </T.Mesh>
+{/each}
