58922dcb3576bf3ce07fb449d5e240cbc20b48e7
Dardaul Hoque <rudro1152@gmail.com>
2025-07-09 16:26:05 +0600

    add footer
    Former-commit-id: 57e26e005205dbd3c5d589e924794ce19a93b3cd

---
+++ src/ui/components/Footer.svelte
@@ -0,0 +1,6 @@
+<div
+  class="text-text-color text-center text-lg fixed left-0 right-0 bottom-2 mx-auto z-100"
+>
+  © 2025. Made by
+  <a href="https://rudro.me" class="text-red-400"><u>rudro.me</u></a>
+</div>
