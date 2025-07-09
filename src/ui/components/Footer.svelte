fde5f960a6a646a68099c69c07ed6de5cef39bb3
Dardaul Hoque <rudro1152@gmail.com>
2025-07-09 16:40:19 +0600

    Display footer conditionally
    Former-commit-id: a8cd44021dec4d644fbe3a1f909a330332c3dadb

--- src/ui/components/Footer.svelte
+++ src/ui/components/Footer.svelte
@@ -1,6 +1,16 @@
-<div
-  class="text-text-color text-center text-lg fixed left-0 right-0 bottom-2 mx-auto z-100"
->
-  © 2025. Made by
-  <a href="https://rudro.me" class="text-red-400"><u>rudro.me</u></a>
-</div>
+<script lang="ts">
+  type Props = {
+    isDesktop: boolean
+  }
+
+  const { isDesktop }: Props = $props()
+</script>
+
+{#if isDesktop}
+  <div
+    class="text-text-color text-center text-lg fixed left-0 right-0 bottom-2 mx-auto"
+  >
+    © 2025. Made by
+    <a href="https://rudro.me" class="text-red-400"><u>rudro.me</u></a>
+  </div>
+{/if}
