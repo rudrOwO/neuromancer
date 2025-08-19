c7b6d388232757a33223e42583d8dc7298959115
Dardaul Hoque <rudro1152@gmail.com>
2025-08-19 20:11:14 +0600

    remove Processing component

--- src/ui/components/Loading.svelte
+++ src/ui/components/Loading.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
-  import Processing from "./Processing.svelte"
+  import brainIconRawString from "/brain-icon.svg?raw"
 
   type Props = {
     message: string
   }
 
   const { message }: Props = $props()
+
+  let paths: NodeListOf<SVGPathElement>
+
+  $effect(() => {
+    paths = document.querySelectorAll("path")
+    paths.forEach((path) => {
+      path.style.setProperty(
+        "--stroke-length",
+        path.getTotalLength().toString(),
+      )
+    })
+  })
 </script>
 
 <div
   class="w-[50vw] sm:w-[15vw] fixed grid place-content-center inset-0 m-auto"
 >
-  <Processing isAnimating={true} />
+  {@html brainIconRawString}
   <p class="mt-4 text-text-color text-2xl sm:text-4xl min-w-max">
     {message}
   </p>
