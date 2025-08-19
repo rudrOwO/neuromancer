4569a0bd1856b621d92971a799a899656fd0310d
Dardaul Hoque <rudro1152@gmail.com>
2025-08-19 19:58:33 +0600

    add linkicon component

---
+++ src/ui/components/LinkIcon.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  type Props = {
+    href: string
+    iconSrc: string
+  }
+
+  const { href, iconSrc }: Props = $props()
+</script>
+
+<a {href} target="_blank" class="w-10 mx-2 sm:mx-6 grid place-items-center">
+  <img class="h-8" src={iconSrc} alt="Github Icon" />
+</a>
