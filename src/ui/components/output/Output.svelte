3da638e54a35da65c01ecd5e182b75ba7572f46e
Dardaul Hoque <rudro1152@gmail.com>
2025-08-19 20:25:05 +0600

    remove conditional rendering from App component

---
+++ src/ui/components/output/Output.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { InferenceResponse } from "bridge"
+  import MobileOutputPanel from "@components/output/Mobile.svelte"
+  import DesktopOutputPanel from "@components/output/Desktop.svelte"
+
+  type Props = {
+    isDesktop: boolean
+    inferenceResponse: InferenceResponse
+    showHint: boolean
+  }
+
+  const { isDesktop, showHint, inferenceResponse }: Props = $props()
+</script>
+
+{#if isDesktop}
+  <DesktopOutputPanel {showHint} {inferenceResponse} />
+{:else}
+  <MobileOutputPanel {showHint} {inferenceResponse} />
+{/if}
