101384dd4daac8ecd04362324737e72aa85b58d5
Dardaul Hoque <rudro1152@gmail.com>
2025-08-26 11:21:55 +0600

    naming is hard
    1.12.8

--- src/ui/components/Error.svelte
+++ src/ui/components/Error.svelte
@@ -9,11 +9,7 @@
 <div
   class="w-[50vw] sm:w-[15vw] fixed grid place-content-center inset-0 m-auto"
 >
-  <img
-    class="w-full"
-    src="/error-icon.svg?inline"
-    alt="Error something went wrong"
-  />
+  <img class="w-full" src="/error-icon.svg" alt="Error something went wrong" />
   <p class="mt-4 text-text-color text-2xl sm:text-4xl min-w-max">
     {message}
   </p>
