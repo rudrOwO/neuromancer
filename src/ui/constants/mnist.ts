96be4bc8ef9339e23c556c86d8c06b61612cc9c4
Dardaul Hoque <rudro1152@gmail.com>
2025-09-04 16:45:31 +0600

    remove useless environment variables

--- src/ui/constants/mnist.ts
+++ src/ui/constants/mnist.ts
@@ -1,7 +1,7 @@
 import { TENSOR_DEFAULT_GRAY_VALUE } from "@constants/graphics"
 import type { InferenceResponse, OutputNode } from "bridge"
 
-export const MODEL_URL = `${import.meta.env.VITE_PUBLIC_URL}/mnist-12.onnx`
+export const MODEL_URL = "/mnist-12.onnx"
 export const INPUT_TENSOR_DIMENSION = [1, 1, 28, 28]
 export const ORDERED_NODE_NAMES = [
   "ReLU32_Output_0",
