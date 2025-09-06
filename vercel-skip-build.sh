a87a0143ae5e62c892fc4e65989167c59d2d7ea2
Dardaul Hoque <rudro1152@gmail.com>
2025-09-06 12:28:13 +0600

    update vercel no-build script

--- vercel-skip-build.sh
+++ vercel-skip-build.sh
@@ -1,4 +1,4 @@
-if [[ $VERCEL_GIT_COMMIT_REF != "main" && $VERCEL_GIT_COMMIT_REF != "staging" ]]; then
+if [[ $VERCEL_GIT_COMMIT_REF != "main" && $VERCEL_GIT_COMMIT_REF != "dev" ]]; then
   echo "Skipping build for $VERCEL_GIT_COMMIT_REF branch"
   exit 0
 fi
