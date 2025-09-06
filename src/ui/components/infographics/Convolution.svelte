742eab2e47ded4973d93263248210870b26ba1e9
Dardaul Hoque <rudro1152@gmail.com>
2025-09-06 11:56:30 +0600

    move scripts from utils/ that are not reused
    Former-commit-id: 260b1990b744ee8143b3bbbcde647835406fb8c0

--- src/ui/components/infographics/Convolution.svelte
+++ src/ui/components/infographics/Convolution.svelte
@@ -4,7 +4,7 @@
     infographicsModal,
     tensorState,
   } from "@sharedstate/infographics.svelte"
-  import { initializeSVG } from "@utils/svgarrows"
+  import { initializeSVG } from "@lib/svgarrows"
   import Aggregate from "./Aggregate.svelte"
   import MaskedTensor from "./MaskedTensor.svelte"
   import UnmaskedTensor from "./UnmaskedTensor.svelte"
--- src/ui/utils/mediaquery.ts
+++
@@ -1,5 +0,0 @@
-export const mediaQuery = window.matchMedia("(min-width: 1280px)")
-
-export const checkIfDesktop = (event: MediaQueryListEvent) => {
-  return event.matches
-}
