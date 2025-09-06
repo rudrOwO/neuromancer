44dca4cf529ed65962f6676aecfd08635c6814e9
Dardaul Hoque <rudro1152@gmail.com>
2025-09-06 10:13:08 +0600

    add MIT license from Microsoft
    Former-commit-id: 229b20e4d5b8ff46ae2b64d67c26737941512716

--- src/ui/utils/math.ts
+++ src/ui/utils/math.ts
@@ -1,4 +1,30 @@
 /**
+ Modified From:: https://github.com/microsoft/onnxruntime-web-demo/blob/3766c1b22aec76c471bb14333b940f6910de4eb1/src/utils/math.ts
+
+ MIT License
+
+ Copyright (c) Microsoft Corporation.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE
+*/
+
+/**
   Find mindpoint of two points
  */
 export function getMidpoint(p1: number[], p2: number[]): number[] {
