d17cb68bca25340a4863b1913fe9172a27e01cd6
Dardaul Hoque <rudro1152@gmail.com>
2025-09-06 11:56:30 +0600

    move scripts from utils/ that are not reused

--- src/ui/components/3d/Tensor2D.svelte
+++ src/ui/components/3d/Tensor2D.svelte
@@ -16,7 +16,6 @@
   } from "@sharedstate/infographics.svelte"
   import { T } from "@threlte/core"
   import { useCursor } from "@threlte/extras"
-  import { getTensorDependencies } from "@utils/tensordeps"
   import type { OutputNode } from "bridge"
   import { AdditiveBlending, Vector3, type Mesh } from "three"
 
@@ -46,8 +45,8 @@
   let hightlighted = $state(false)
   const bufferGeometryLength = 3 * rows * columns
   const vertices = new Float32Array(bufferGeometryLength)
-
   const worldPosition = new Vector3()
+  const { onPointerEnter, onPointerLeave } = useCursor()
 
   // Enumerating vertices
   // 3 consecutive values define one vertex (x, y, z)
@@ -99,7 +98,43 @@
     }
   })
 
-  const { onPointerEnter, onPointerLeave } = useCursor()
+  function getTensorDependencies(
+    layerName: LayerName,
+    tensorIndex: number,
+    previousLayerActivationMaps: Array<Float32Array>,
+  ): {
+    dependencyTensors: Float32Array[]
+    dependencyTensorLocations: Array<Vector3>
+  } {
+    switch (layerName) {
+      case "Convolution Layer #1":
+        return {
+          dependencyTensors: previousLayerActivationMaps,
+          dependencyTensorLocations: tensorLocationMatrix["Input"],
+        }
+      case "Max Pool #1":
+        return {
+          dependencyTensors: [previousLayerActivationMaps[tensorIndex]],
+          dependencyTensorLocations: [
+            tensorLocationMatrix["Convolution Layer #1"][tensorIndex],
+          ],
+        }
+      case "Convolution Layer #2":
+        return {
+          dependencyTensors: previousLayerActivationMaps,
+          dependencyTensorLocations: tensorLocationMatrix["Max Pool #1"],
+        }
+      case "Max Pool #2":
+        return {
+          dependencyTensors: [previousLayerActivationMaps[tensorIndex]],
+          dependencyTensorLocations: [
+            tensorLocationMatrix["Convolution Layer #2"][tensorIndex],
+          ],
+        }
+    }
+
+    throw "Unexpected Error: Layer name is wrong"
+  }
 
   function handleClick() {
     if (layerName != "Input") {
